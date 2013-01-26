a95df46b63bcb5cc4ea0a36786857f2a09e01aed
katryo <katoryo55@gmail.com>
2013-01-26 19:02:30 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kansoguild::Application.config.secret_token = '3c584e4cd8275405a08e3fde6219ad837da9cee102871b9db2eb25e7ea74e13e1823e02e37aa4d0e5de82cd2da5792358f3ce60690b05690e1b146eb0fbdb378'
