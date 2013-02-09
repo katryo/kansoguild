e355617566d71ddf30e6ed52efe96ed7492f2ba9
katryo <katoryo55@gmail.com>
2013-02-09 22:41:54 +0900

    herokuデプロイ直前

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,40 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
   validates_presence_of :username
+  validates_uniqueness_of :username
   # attr_accessible :title, :body
+
+  def self.from_omniauth(auth)
+    if session["devise.user_attributes"]    
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.username = auth.info.nickname
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
+
+  def email_required?
+    super && provider.blank?
+  end
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Kansoguild::Application.routes.draw do
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
+     controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
   resources "items"
 
